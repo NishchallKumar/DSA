b95b0f6f805625e58ea11be5139a6489ab1b5df9
NishchallKumar <144469837+NishchallKumar@users.noreply.github.com>
2025-01-15 20:54:39 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,53 +1,19 @@
 class reverse{
-
-    public static void rev(int[] arr){
-        int e = arr.length-1;
-        int s = 0;
-        while(s<e){
-            int t = arr[s];
-            arr[s] = arr[e];
-            arr[e] = t;
-            s++;
-            e--;
+    public static void reverseArr(int [] arr, int ft, int lt){
+        while(ft<lt){
+            int temp = arr[ft];
+            arr[ft] = arr[lt];
+            arr[lt] = temp;
+            ft++;
+            lt--;
         }
-        System.out.println("This is reversed Array:");
-        for(int i : arr){
-            System.out.print(i + " ");
-        }
-        System.err.println();
     }
-
-    public static void max(int[] arr){
-        int ans = 0;
-        for(int i : arr){
-            if (i>ans){
-                ans = i;
-            }
-        }
-        System.out.println(ans);
-    }
-
-    public static void min(int[] arr){
-        int ans = 0;
-        for(int i : arr){
-            if (i<ans){
-                ans = i;
-            }
-        }
-        System.out.println(ans);
-    }
-    
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9,0};
-        System.out.println("This is original Array:");
+        int arr[] = {10,20,30,40,50,60,70};
+        reverseArr(arr,2,5);
+        reverseArr(arr,3,6);
         for(int i : arr){
-            System.out.print(i + " ");
+            System.out.println(i);
         }
-        System.err.println();
-        rev(arr);
-        System.out.println("This is maximum of Array:");
-        max(arr);
-        System.out.println("This is minimum of Array:");
-        min(arr);
     }
 }
